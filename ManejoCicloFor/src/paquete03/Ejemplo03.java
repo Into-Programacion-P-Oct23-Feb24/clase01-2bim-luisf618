1e6d8ca98f8b49170a7bfee1152892203ca34451
Luis Cordova <lcordovacarrion@gmail.com>
2023-12-06 16:29:06 -0500

    Ciclo For

--- ManejoCicloFor/src/paquete03/Ejemplo03.java
+++ ManejoCicloFor/src/paquete03/Ejemplo03.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package paquete03;
-
+import java.util.Scanner;
+import java.util.Locale;
 /**
  *
  * @author reroes
@@ -12,9 +13,39 @@ package paquete03;
 public class Ejemplo03 {
     
     public static void main(String[] args) {
-        
-        for (int i = 0; i < 10; i++) {
-            
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        int limite;
+        String cadenaReporte = "";
+        String nombre;
+        String posicion;
+        int edad;
+        double estatura;
+        System.out.println("Cuantos jugadores desea ingresar");
+        limite = entrada.nextInt();
+        for (int i = 1; i <= limite; i++) {
+            System.out.printf("Ingrese el nombre del jugador %s:\n",
+                    i);
+            entrada.nextLine();
+            nombre = entrada.nextLine();
+            System.out.printf("Ingrese la posición del jugador %s:\n",
+                    i);
+            posicion = entrada.nextLine();
+            System.out.printf("Ingrese la edad del jugador %s:\n",
+                    i);
+            edad = entrada.nextInt();
+            System.out.printf("Ingrese la estatura del jugador %s:\n",
+                    i);
+            estatura = entrada.nextDouble();
+            cadenaReporte = String.format("%s%d. %s -%s-, edad %d,"
+                    + " estatura %.2f\n",
+                    cadenaReporte,
+                    i,
+                    nombre,
+                    posicion,
+                    edad,
+                    estatura);
         }
+        System.out.printf("%s", cadenaReporte);
     }
 }
